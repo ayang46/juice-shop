c91aa4676c05ff57322c6635a09cdefb563b40e2
ayang46 <ayang46@asu.edu>
2026-03-08 09:40:54 -0700

    Fix login sql injection authentication flow

--- routes/login.ts
+++ routes/login.ts
@@ -31,8 +31,14 @@ export function login () {
 
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
-    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
-      .then((authenticatedUser) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
+    //models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
+      // fixed login sql injection vulnerability - vuln-code-snippet
+      models.User.findOne({ where: {
+          email: req.body.email || "",
+          password: security.hash(req.body.password || ""),
+          deletedAt: null
+        }
+      }).then((authenticatedUser) => {// vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
         const user = utils.queryResultToJson(authenticatedUser)
         if (user.data?.id && user.data.totpSecret !== '') {
           res.status(401).json({
